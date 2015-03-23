af42419509ea8f450d6f4328b556f297775c1b79
Abderrahman Moussaoui <hmani@soufnet.com>
2015-03-23 15:03:29 +0100

    first init على بركة الله

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('default', function() {
+    // place code for your default task here
+});
+gulp.task('sass', function () {
+    gulp.src('./app/scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./app/style/css'));
+});
+// watch files for changes and reload
+gulp.task('serve', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        }
+    });
+    gulp.watch('./app/scss/*.scss', ['sass']);
+    gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js','./app/scss/*.scss'], {cwd: 'app'}, reload);
+});
